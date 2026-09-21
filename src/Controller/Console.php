71c1ebca5e490573050afa7676abc4461e8e0023
Xakki <git@xakki.ru>
2026-09-22 00:53:38 +0300

    Extract shared send/reSend loop into Console::processQueue()

--- src/Controller/Console.php
+++ src/Controller/Console.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Xakki\Emailer\Controller;
 
 use Xakki\Emailer\Cqrs\Helper\Migration;
+use Xakki\Emailer\Cqrs\Queue\AbstractQueue;
 use Xakki\Emailer\Cqrs\Queue\ExecuteQueue;
 use Xakki\Emailer\Cqrs\Queue\RepeatQueue;
 use Xakki\Emailer\Cqrs\Transport\NewDayTransport;
@@ -20,39 +21,21 @@ class Console extends AbstractController
 {
     protected function actionSend(int $repeat = 1): string
     {
-        $info = [];
-        $stopFlag = false;
-        for ($i = 0; $i < $repeat; $i++) {
-            $db = $this->emailer->getDb();
-            $db->beginTransaction();
-            try {
-                $status = (new ExecuteQueue($this->emailer))->handler();
-                $mess = Queue::TITLE_QUEUE_STATUS[$status];
-                $db->commit();
-            } catch (DataNotFound $e) {
-                $db->rollBack();
-                if ($e->httpCode === 0) {
-                    break;
-                }
-                throw $e;
-            } catch (\Throwable $e) {
-                $this->logger->error($e);
-                $db->rollBack();
-                $mess = $e->getMessage();
-                $stopFlag = true;
-            }
-            if (!isset($info[$mess])) {
-                $info[$mess] = 0;
-            }
-            $info[$mess]++;
-            if ($stopFlag) {
-                break;
-            }
-        }
-        return 'Statuses: ' . var_export($info, true);
+        return $this->processQueue(fn(): ExecuteQueue => new ExecuteQueue($this->emailer), $repeat);
     }
 
     protected function actionReSend(int $repeat = 1): string
+    {
+        return $this->processQueue(fn(): RepeatQueue => new RepeatQueue($this->emailer), $repeat);
+    }
+
+    /**
+     * Shared send/reSend loop: $factory selects the next row (throwing
+     * DataNotFound with httpCode 0 when the queue is drained).
+     *
+     * @param callable(): AbstractQueue $factory
+     */
+    private function processQueue(callable $factory, int $repeat): string
     {
         $info = [];
         $stopFlag = false;
@@ -60,7 +43,7 @@ class Console extends AbstractController
             $db = $this->emailer->getDb();
             $db->beginTransaction();
             try {
-                $status = (new RepeatQueue($this->emailer))->handler();
+                $status = $factory()->handler();
                 $mess = Queue::TITLE_QUEUE_STATUS[$status];
                 $db->commit();
             } catch (DataNotFound $e) {
